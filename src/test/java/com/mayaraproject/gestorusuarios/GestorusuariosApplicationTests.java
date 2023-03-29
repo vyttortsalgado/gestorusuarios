6e833392c5dab186d77b63881e2303a0024e9d41
Vyttor Salgado <vyttortsalgado@hotmail.com>
2023-03-29 20:11:57 -0300

    Criando a base do projeto

---
+++ src/test/java/com/mayaraproject/gestorusuarios/GestorusuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayaraproject.gestorusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorusuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
